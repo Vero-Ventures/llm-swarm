6895672d22ab0796f1bedd5efeda28212f0eaa38
Terence Grigoruk <terence.grigoruk@gmail.com>
2024-05-16 16:13:00 -0700

    fix: wait a bit after starting the server

--- python-package/src/package/ai/models.py
+++ python-package/src/package/ai/models.py
@@ -1,6 +1,14 @@
 import subprocess
+import time
 import ollama
 from langchain_community.llms import Ollama
+
+
+def get_local_models():
+    """
+    Get the list of locally saved models.
+    """
+    return [model["name"] for model in ollama.list().get("models")]
 
 
 def download_model(model_name: str):
@@ -8,33 +16,34 @@ def download_model(model_name: str):
     Download the model to local machine (saves to `~/.ollama/models` by default).
     """
     try:
-        # start the ollama server - necessary for using the ollama package
-        process = subprocess.Popen(
+        # Start the ollama server (necessary for using the ollama package)
+        # and let continue running in the background
+        subprocess.Popen(
             ["ollama", "serve"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            shell=True,
+            # shell=True,
         )
+        # give the server some time to start
+        time.sleep(3)
+
         print("Ollama server started.")
     except (FileNotFoundError, subprocess.CalledProcessError):
         print("Ollama is not installed. Please visit https://ollama.com/.")
         return
 
-    locally_saved_models = [model["name"] for model in ollama.list().get("models")]
-    if model_name not in locally_saved_models:
+    print("Checking if model exists locally...")
+    if model_name not in get_local_models():
         try:
             print(f"Downloading model {model_name}...")
             ollama.pull(model_name)
         except ollama._types.ResponseError:
             print(f"Model {model_name} could not be retrieved.")
             return False
+    else:
+        print(f"Model {model_name} already exists locally.")
 
-    # stop the ollama server
-    process.terminate()
-    print("Ollama server stopped.")
-
-    # TODO: check if the model exists locally
     return True
 
 
